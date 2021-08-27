5b6d3221e570da0e4376b1a9b717db776bdc9a38
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-08-28 01:30:22 +0200

    added courses-card-list presentational component to separate the data managin of the data dsplaying

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -40,10 +40,7 @@ export class HomeComponent implements OnInit {
 
   advancedCourses$: Observable<Course[]>;
 
-  constructor(
-    private coursesService: CoursesService,
-    private dialog: MatDialog
-  ) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
     const courses$: Observable<Course[]> =
@@ -67,21 +64,6 @@ export class HomeComponent implements OnInit {
           (course) => (course.category = 'ADVANCED')
         )
       )
-    );
-  }
-
-  editCourse(course: Course) {
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = '400px';
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(
-      CourseDialogComponent,
-      dialogConfig
     );
   }
 }
