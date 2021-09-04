eb6a24d44344aa77ff2e06451ae053be30c019fa
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-09-04 20:16:16 +0200

    added state mnagement to the user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
-
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthStore } from '../services/auth.store';
 
 @Component({
   selector: 'login',
@@ -10,30 +14,34 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private router: Router) {
-
+    private router: Router,
+    private auth: AuthStore
+  ) {
     this.form = fb.group({
-      email: ['test@angular-university.io', [Validators.required]],
+      email: [
+        'test@angular-university.io',
+        [Validators.required]
+      ],
       password: ['test', [Validators.required]]
     });
-
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   login() {
-
     const val = this.form.value;
 
-
-
+    this.auth.login(val.email, val.password).subscribe(
+      () => {
+        this.router.navigate(['/courses']);
+      },
+      (err) => {
+        alert('login Failed!');
+      }
+    );
   }
-
 }
